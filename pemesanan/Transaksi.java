69e4b4a351e68d40b80b0befd430aa2915599bdc
Nomu1Nomu1 <isaiah.tino@gmail.com>
2025-11-07 13:05:56 +0700

    muehe

--- pemesanan/Transaksi.java
+++ pemesanan/Transaksi.java
@@ -1,5 +1,22 @@
 package pemesanan;
 
 public class Transaksi {
-    
+    // Inisialisasi namaPelanggan, busDipilih, nomorKursi, paketMakan, totalHarga, pajak, idTransaksi
+    // Kurang lebih sama seperti pada Bus.java
+
+    public Transaksi() {
+
+    }
+
+    // Function untuk menghitung total transaksi dan harga sudah termasuk pajak dan juga user mau menambah paket makan
+    private void hitungTotal() {
+
+    }
+
+    // Getter
+
+    // Membuat rute
+    public String getRute() {
+        
+    }
 }
