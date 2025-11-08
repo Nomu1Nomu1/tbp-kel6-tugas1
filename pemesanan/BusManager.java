620a1fbd48dadeaf84daa1fbd2a40900fe4ebba4
Nomu1Nomu1 <isaiah.tino@gmail.com>
2025-11-08 19:10:59 +0700

    waduh gweh lupa kalo ada member wowow

--- pemesanan/BusManager.java
+++ pemesanan/BusManager.java
@@ -7,9 +7,17 @@ public class BusManager {
     // Inisialisasi daftarBus menggunakan ArrayList, daftarTransksi menggunakan
     // ArrayList, Scanner, dan idCounter
     // Sama seperti yang di Bus.java, cuman nggak dimasukin di dalam '()'
+    private ArrayList<Bus> daftarBus;
+    private ArrayList<Transaksi> dafTransaksi;
+    private Scanner scanner;
+    private int idCounter;
 
     public BusManager() {
-
+        this.daftarBus = new ArrayList<>();
+        this.dafTransaksi = new ArrayList<>();
+        this.scanner = new Scanner(System.in);
+        this.idCounter = 1001;
+        inisialisasiDataBus();
     }
 
     // Inisialisasi data bus seperti nama bus, jenis bus, asal keberangkatan, Tujuan
@@ -45,7 +53,7 @@ public class BusManager {
 
     // Scanner 
     public Scanner getScanner() {
-        return Scanner;
+        return scanner;
     }
 
     public boolean isExit(int pilihan) {
--- pemesanan/Transaksi.java
+++ pemesanan/Transaksi.java
@@ -3,9 +3,21 @@ package pemesanan;
 public class Transaksi {
     // Inisialisasi namaPelanggan, busDipilih, nomorKursi, paketMakan, totalHarga, pajak, idTransaksi
     // Kurang lebih sama seperti pada Bus.java
+    private String namaPelanggan;
+    private Bus busDipilih;
+    private int nomorKursi;
+    private boolean paketMakan;
+    private double totalHarga;
+    private double pajak;
+    private String idTransaksi;
 
-    public Transaksi() {
-
+    public Transaksi(String nama, Bus bus, int kursi, boolean makan, String id) {
+        this.namaPelanggan = nama;
+        this.busDipilih = bus;
+        this.nomorKursi = kursi;
+        this.paketMakan = makan;
+        this.idTransaksi = id;
+        hitungTotal();
     }
 
     // Function untuk menghitung total transaksi dan harga sudah termasuk pajak dan juga user mau menambah paket makan
@@ -14,6 +26,8 @@ public class Transaksi {
     }
 
     // Getter
+    public String getIdTransaksi() { return idTransaksi; }
+    
 
     // Membuat rute
     public String getRute() {
